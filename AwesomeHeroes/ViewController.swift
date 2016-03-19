ed6affb846b0731344ef9925ebdac774248d9b32
Alberto Moral <alberto.moral.g@gmail.com>
2016-03-19 10:19:06 +0100

    Initial Commit

---
+++ AwesomeHeroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwesomeHeroes
+//
+//  Created by Alberto Moral on 19/3/16.
+//  Copyright © 2016 Alberto Moral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
